b01b4c682289d7d90ca94f3889c293a5b7ac8bda
Rishi Ram Rijal <ramrijalrishi@gmail.com>
2025-09-30 17:03:39 +0200

    fix the app component

--- src/main.jsx
+++ src/main.jsx
@@ -37,6 +37,7 @@ const router = createBrowserRouter([
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
+      <App/>
       <RouterProvider router={router} />
     </Provider>
   </StrictMode>
