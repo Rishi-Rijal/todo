055215e10c6a0ea8e142f3fa1d70488cf3d8cc8c
Rishi Ram Rijal <ramrijalrishi@gmail.com>
2025-10-01 08:20:29 +0200

    added todo and subtodo api

--- src/api/axios.api.js
+++ src/api/axios.api.js
@@ -5,4 +5,14 @@ const userApi = axios.create({
   withCredentials: true,  
 });
 
-export {userApi}
+const todoApi = axios.create({
+  baseURL: import.meta.env.VITE_TODO_API_URL,
+  withCredentials: true
+})
+
+const subtodoApi = axios.create({
+  baseURL: import.meta.env.VITE_SUBTODO_API_URL,
+  withCredentials: true
+})
+
+export {userApi, todoApi, subtodoApi}
